ab7eb2ba76b28413cb3c474cf216a16a34cd4f8a
Mahmoud2000-ali <mali49837@gmail.com>
2022-10-11 17:15:08 +0300

    Code

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Mail\test;
+use App\Mail\TestMail;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/send' , function(){
+    Mail::to('mahmoudalielnahal@gmail.com')->send(new TestMail());
+
+    return response('sending');
+});
